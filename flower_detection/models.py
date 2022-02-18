fafbae5194a4e6cfe806e53148f9c6a6634576fd
ACE-PROJECTS-21 <aceprojects21@gmail.com>
2022-02-18 19:43:23 +0530

    18-02-2022

--- flower_detection/models.py
+++ flower_detection/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 
-class Members(models.Model):
-    fname = models.CharField(max_length=50)
-    lname = models.CharField(max_length=50)
-    email = models.EmailField(max_length=200)
-    passwd = models.CharField(max_length=50)
-
+class Info(models.Model):
+    flower_name = models.CharField(max_length=100)
+    introduction = models.TextField()
+    etymology = models.TextField()
+    evolution = models.TextField()
+    species = models.TextField()
+    tax_domain = models.CharField(max_length=100)
+    tax_kingdom = models.CharField(max_length=100)
+    tax_phylum = models.CharField(max_length=100)
+    tax_order = models.CharField(max_length=100)
+    tax_family = models.CharField(max_length=100)
+    tax_genus = models.CharField(max_length=100)
+    ornamental_plants = models.TextField()
+    cut_flowers = models.TextField()
+    medicinal_uses = models.TextField()
+    health_benefits = models.TextField()
+    perfumes_food = models.TextField()
+    symbolizes = models.TextField()
+    season_to_grow = models.TextField()
+    
 def __str__(self):
-    return self.fname + '' + self.lname
+    return self.flower_name 
